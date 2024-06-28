c22455f97e465f6fe437879a7e3a269b4ee11729
milkyaz <139978789+milkyaz@users.noreply.github.com>
2024-06-29 00:45:32 +0400

    заменил на массив объектов

--- JS/mainOnePage.js
+++ JS/mainOnePage.js
@@ -6,37 +6,35 @@ function getData() {
   let descriptionBook = document.getElementById("description-book").value;
   let rate = document.getElementById("rate").value;
 
-  // getting existing values from local storage or creating new arrays if they don't exist
-  let bookNames = JSON.parse(localStorage.getItem("book-names")) || [];
-  let descriptionBooks =
-    JSON.parse(localStorage.getItem("description-books")) || [];
-  let rates = JSON.parse(localStorage.getItem("rates")) || [];
-  let images = JSON.parse(localStorage.getItem("images")) || [];
+  // getting existing values from local storage or creating a new array if it doesn't exist
+  let books = JSON.parse(localStorage.getItem("books")) || [];
 
-  // adding new values to arrays
-  bookNames.push(bookName);
-  descriptionBooks.push(descriptionBook);
-  rates.push(rate);
+  // creating a new book object and adding it to the array
+  let newBook = {
+    name: bookName,
+    description: descriptionBook,
+    rate: rate,
+    image: null,
+  };
+
   if (localStorage.getItem("img-BASE64")) {
-    images.push(localStorage.getItem("img-BASE64"));
+    newBook.image = localStorage.getItem("img-BASE64");
     localStorage.removeItem("img-BASE64");
-  } else {
-    images.push(null);
   }
 
-  // saving arrays in local storage
-  localStorage.setItem("book-names", JSON.stringify(bookNames));
-  localStorage.setItem("description-books", JSON.stringify(descriptionBooks));
-  localStorage.setItem("rates", JSON.stringify(rates));
-  localStorage.setItem("images", JSON.stringify(images));
+  books.push(newBook);
+
+  // saving the array of book objects in local storage
+  localStorage.setItem("books", JSON.stringify(books));
 
   // incrementing book index
   bookIndex++;
 }
+
 //Функция преобразует файл в в base64
 function getBase64(file) {
   return new Promise((resolve, reject) => {
-    var reader = new FileReader();
+    let reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = function () {
       resolve(reader.result);
